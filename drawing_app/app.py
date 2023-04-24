26f406a9b13d0451a10b262cea799eb6cecc838c
Samuel Wierzbicki <wierzbickisamuel@gmail.com>
2023-04-24 21:14:54 +0200

    Develop drawing app

--- drawing_app/app.py
+++ drawing_app/app.py
@@ -1,43 +1,67 @@
+import imageio
 from tkinter import *
-
-window = Tk()
-window.title('Sketchbook')
-window.geometry('500x500')
-
-# Crate Canvas widget
-my_canvas = Canvas(window, width=8, height=8, bg='white')
-# Pack Canvas to thinket objet, anchow - starting point (north-west, (0, 0)); fill and expand  - stretch canvas to the grids
-my_canvas.pack(anchor='nw', fill='both', expand=1)
-
-numb = list()
+import pickle
+from PIL import ImageTk, Image, ImageDraw
+import PIL
+import numpy as np
+import pyscreenshot as ImageGrab
+import io
 
 
-def initial_cordinates(event):
-    global x_cord, y_cord
-    x_cord, y_cord = event.x, event.y
-    cords = x_cord, y_cord
-    numb.append(cords)
+class App(object):
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Sketchbook')
+        self.window.geometry('500x500')
+        self.my_canvas = Canvas(self.window, width=500, height=500, bg='white')
+
+        self.number_label = Label(self.window, text='PREDICTED: NONE')
+
+        self.number_label.place(relx=0.0, rely=1.0, anchor='sw')
+        self.my_canvas.pack()
+
+        self.setup()
+        self.load_model('./model.pkl')
+        self.window.mainloop()
+
+    def setup(self):
+        self.my_canvas.bind("<Button-1>", self.initial_cordinates)
+        self.my_canvas.bind("<B1-Motion>", self.draw)
+        self.my_canvas.bind("<Button-3>", self.clear_sketchbook)
+        self.my_canvas.bind("<Button-2>", self.predict_number)
+
+    def initial_cordinates(self, event):
+        global x_cord, y_cord
+        self.x_cord, self.y_cord = event.x, event.y
+
+    def draw(self, event):
+        global x_cord, y_cord
+        self.my_canvas.create_line((self.x_cord, self.y_cord, event.x, event.y),
+                                   fill='black', width=8)
+        self.x_cord, self.y_cord = event.x, event.y
+
+    def clear_sketchbook(self, event):
+        self.my_canvas.delete('all')
+        self.number_label.config(text='PREDICTED: NONE')
+
+    def load_model(self, model_path):
+        with open(model_path, 'rb') as f:
+            self.model = pickle.load(f)
+        return self.model
+
+    def predict_number(self, event):
+        self.canvas_postscript = self.my_canvas.postscript(colormode='color')
+        self.image = Image.open(io.BytesIO(
+            self.canvas_postscript.encode('utf-8')))
+        self.image = self.image.resize((8, 8))
+        # Convert to grayscale
+        self.image = self.image.convert('L')
+        self.image = np.array(self.image)
+        self.image = self.image.reshape([1, 64])
+        self.prediction = self.model.predict(self.image)
+        # self.label = np.argmax(self.prediction, axis=1)
+        self.number_label.config(text='PREDICTED:' + str(self.prediction))
 
 
-def draw(event):
-    global x_cord, y_cord
-
-    my_canvas.create_line((x_cord, y_cord, event.x, event.y),
-                          fill='black', width=8)
-    x_cord, y_cord = event.x, event.y
-    cords = x_cord, y_cord
-    numb.append(cords)
-
-
-def clear_sketchbook(event):
-    my_canvas.delete('all')
-    numb.clear()
-
-
-# Interaction with user
-my_canvas.bind("<Button-1>", initial_cordinates)
-my_canvas.bind("<B1-Motion>", draw)
-my_canvas.bind("<Button-3>", clear_sketchbook)
-
-
-window.mainloop()
+if __name__ == '__main__':
+    App()
